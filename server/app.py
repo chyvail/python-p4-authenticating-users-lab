4e3b7f09cb5d257deb3676920b8363e9ec959988
joshua chivile <joshuachivile@gmail.com>
2024-01-22 19:49:06 +0300

    Authenticating users working

--- server/app.py
+++ server/app.py
@@ -18,6 +18,34 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return jsonify(user.to_dict())
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return jsonify(user.to_dict())
+        else:
+            return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+api.add_resource(Logout, '/logout')
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -48,6 +76,7 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
